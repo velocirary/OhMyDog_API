d17a47d7fa531351e47120d34d640e3817a5c3a6
Rary <larissa.proenca@fatec.sp.gov.br>
2023-09-01 08:30:27 -0300

    feat(ClienteController): Add Post, Patch Clientes
    Adicionando opção de Post e Patch para Clientes

--- OhMyDog_API/Model/Clientes/LoginCliente.cs
+++ OhMyDog_API/Model/Clientes/LoginCliente.cs
@@ -5,5 +5,25 @@
         public string CodCliente { get; set; }
 
         public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public DateTime? DataNasc { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Bairro { get; set;}
+
+        public string Municipio { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Celular { get; set; }
     }
 }
