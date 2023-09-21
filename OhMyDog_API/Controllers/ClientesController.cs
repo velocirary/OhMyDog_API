797cc42243a0622cd9a3f7259d80206987db11ff
Rary <larissa.proenca@fatec.sp.gov.br>
2023-09-20 21:41:57 -0300

    fix(ClientesController); Alteração Cliente

--- OhMyDog_API/Controllers/ClientesController.cs
+++ OhMyDog_API/Controllers/ClientesController.cs
@@ -4,6 +4,7 @@ using static OhMyDog_API.Model.Parameters;
 using System.Web;
 using OhMyDog_API.Model.Clientes;
 using OhMyDog_API.Model.Pets;
+using static OhMyDog_API.Controllers.ConexaoController;
 
 namespace OhMyDog_API.Controllers
 {
@@ -17,7 +18,7 @@ namespace OhMyDog_API.Controllers
         {
             try
             {
-                var loginCliente = new LoginCliente();
+                var loginCliente = new DadosClientes();
 
                 string queryString = "SELECT * FROM Cliente";
                 OdbcCommand command = new OdbcCommand(queryString, connection);
@@ -27,6 +28,16 @@ namespace OhMyDog_API.Controllers
                 {
                     loginCliente.CodCliente = reader["codCliente"].ToString();
                     loginCliente.Nome = reader["Nome"].ToString();
+                    loginCliente.CPF = reader["CPF"].ToString();
+                    loginCliente.DataNasc = Convert.ToDateTime( reader["DataNascimento"]);
+                    loginCliente.CEP = reader["CEP"].ToString();
+                    loginCliente.Logradouro = reader["Logradouro"].ToString();
+                    loginCliente.Bairro = reader["Bairro"].ToString();
+                    loginCliente.Municipio = reader["Municipio"].ToString();
+                    loginCliente.Estado = reader["Estado"].ToString();
+                    loginCliente.Email = reader["Email"].ToString();
+                    loginCliente.Senha = reader["Senha"].ToString();
+                    loginCliente.Celular = reader["Celular"].ToString();
                 }
 
                 return Ok(loginCliente);
@@ -38,8 +49,31 @@ namespace OhMyDog_API.Controllers
         }
 
         [HttpPost]
+        [Route("LoginCliente")]
+        public async Task<IActionResult> PostLogarCliente([FromBody] LoginCliente login)
+        {
+            try
+            {
+                var loginCliente = new DadosClientes();
+
+                OdbcDataReader reader = ExecutaQuery($"SELECT * FROM Cliente WHERE Email = '{login.Email}' and Senha = '{login.Senha}'");
+
+                if (reader.Read())
+                {
+                    return Ok("{\r\n  \"codCliente\":" + reader["codCliente"].ToString() + "\r\n}");
+                }
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        [HttpPost]
         [Route("InserirNovoCliente")]
-        public async Task<IActionResult> PostNovoCliente([FromBody] LoginCliente cliente)
+        public async Task<IActionResult> PostNovoCliente([FromBody] DadosClientes cliente)
         {
             try
             {
@@ -58,9 +92,11 @@ namespace OhMyDog_API.Controllers
             }
         }
 
+
+
         [HttpPatch]
         [Route("AtualizarCliente")]
-        public async Task<IActionResult> AtualizarCliente([FromBody] LoginCliente cliente)
+        public async Task<IActionResult> AtualizarCliente([FromBody] DadosClientes cliente)
         {
             try
             {
@@ -98,7 +134,7 @@ namespace OhMyDog_API.Controllers
                 string queryString = $"SELECT codCliente FROM Pet WHERE codCliente = '{codCliente}'";
                 OdbcCommand command = new OdbcCommand(queryString, connection);
 
-                queryString = $"DELETE FROM Cliente WHERE codPet ='{codCliente}'";
+                queryString = $"DELETE FROM Cliente WHERE codCliente ='{codCliente}'";
                 command = new OdbcCommand(queryString, connection);
                 command.ExecuteReader();
 
