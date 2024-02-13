654663d8f11b099d70e894d19ac5064c8c0b0f35
DaniloGabrielGuedes <88410319+velocirary@users.noreply.github.com>
2024-02-13 18:54:11 -0300

    fix(DadosPet): Inserção Campos Dados Pet

--- OhMyDog_API/Model/Pets/DadosPets.cs
+++ OhMyDog_API/Model/Pets/DadosPets.cs
@@ -9,6 +9,15 @@ namespace OhMyDog_API.Model.Pets
         public string Cor { get; set; }
         public string Nome { get; set; }
         public string Especie { get; set; }
+        public string Peso { get; set; }
+        
+        private string _castrado;
+        public string Castrado
+        {
+            get { return _castrado; }
+            set { _castrado = value == "True" ? "1" : "0"; }
+        }
+        public string Observacao { get; set; }
         public string codCliente { get; set; }
     }
 }
