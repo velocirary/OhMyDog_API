764135b3c901dd2f32de740f1b3fda7edbdaa7dd
DaniloGabrielGuedes <88410319+velocirary@users.noreply.github.com>
2024-02-13 18:52:21 -0300

    fix(PetsController): Correções e Inclusões

--- OhMyDog_API/Controllers/PetsController.cs
+++ OhMyDog_API/Controllers/PetsController.cs
@@ -5,6 +5,7 @@ using static OhMyDog_API.Model.Parameters;
 using static OhMyDog_API.Controllers.ConexaoController;
 using OhMyDog_API.Model.Clientes;
 using System.Reflection.PortableExecutable;
+using System.Data;
 
 namespace OhMyDog_API.Controllers
 {
@@ -21,19 +22,83 @@ namespace OhMyDog_API.Controllers
                 DadosPets pet;
                 List<DadosPets> pets = new List<DadosPets>();
 
-                OdbcDataReader reader = ExecutaQuery("SELECT * FROM pet");
+                var table = ExecutaQuery("SELECT * FROM pet");
 
-                while (reader.Read())                
+                foreach (DataRow reader in table.Rows)
+                {
                     pets.Add(new DadosPets
                     {
-                        codPet = reader["codCliente"].ToString(),
+                        codPet = reader["codPet"].ToString(),
                         Sexo = reader["Sexo"].ToString(),
                         Cor = reader["Cor"].ToString(),
                         Nome = reader["Nome"].ToString(),
                         Especie = reader["Especie"].ToString(),
+                        Peso = reader["Peso"].ToString(),
+                        Castrado = reader["Castrado"].ToString(),
+                        Observacao = reader["Observacao"].ToString(),
                         codCliente = reader["codCliente"].ToString()
-                    });                    
-                
+                    });
+                }
+
+                return Ok(pets);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        [HttpGet]
+        [Route("Pet/{codPet}")]
+        public async Task<IActionResult> RetornarPet([FromRoute] string codPet)
+        {
+            try
+            {
+                DadosPets pet = null;
+
+                DataTable table = ExecutaQuery($"SELECT * FROM pet WHERE codPet = '{codPet}'");
+                foreach (DataRow row in table.Rows)
+                {
+                    pet = new DadosPets
+                    {
+                        codPet = row["codPet"].ToString(),
+                        Sexo = row["Sexo"].ToString(),
+                        Cor = row["Cor"].ToString(),
+                        Nome = row["Nome"].ToString(),
+                        Especie = row["Especie"].ToString(),
+                        Peso = row["Peso"].ToString(),
+                        Castrado = row["Castrado"].ToString(),
+                        Observacao = row["Observacao"].ToString(),
+                        codCliente = row["codCliente"].ToString()
+                    };
+                }
+
+                return Ok(pet);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        [HttpGet]
+        [Route("PetPorCliente/{codCliente}")]
+        public async Task<IActionResult> RetornarPetPorCliente([FromRoute] string codCliente)
+        {
+            try
+            {
+                DataTable table = ExecutaQuery($"SELECT * FROM pet WHERE codCliente = '{codCliente}'");
+
+                List<DadosPets> pets = new();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    pets.Add(new DadosPets
+                    {
+                        codPet = row["codPet"].ToString(),
+                        Nome = row["Nome"].ToString()
+                    });
+                }
 
                 return Ok(pets);
             }
@@ -51,8 +116,10 @@ namespace OhMyDog_API.Controllers
             {
                 if (pet == null)
                     return BadRequest();
+                
+                DataTable table = ExecutaQuery($"SELECT MAX(codPet) + 1 FROM pet");
 
-                ExecutaQuery($"INSERT INTO pet (codPet, Sexo, Cor, Nome, Especie, codCliente) VALUES ('{pet.codPet}', '{pet.Sexo}', '{pet.Cor}', '{pet.Nome}', '{pet.Especie}', '{pet.codCliente}')");                
+                ExecutaQuery($"INSERT INTO pet (codPet, Sexo, Cor, Nome, Especie, Observacao, Castrado, Peso, codCliente) VALUES ('{table.Rows[0][0]}', '{pet.Sexo}', '{pet.Cor}', '{pet.Nome}', '{pet.Especie}', '{pet.Observacao}', '{pet.Castrado}', '{pet.Peso}', '{pet.codCliente}')");
 
                 return Ok();
             }
@@ -69,9 +136,9 @@ namespace OhMyDog_API.Controllers
             try
             {
                 string queryString = $"SELECT codPet FROM Pet WHERE codPet = '{codPet}'";
-                OdbcDataReader reader = ExecutaQuery(queryString);
+                DataTable table = ExecutaQuery(queryString);
 
-                if (!reader.Read())
+                if (table.Rows.Count == 0)
                     return BadRequest("Pet não encontrado!");
 
                 queryString = $"DELETE FROM Pet WHERE codPet ='{codPet}'";
@@ -96,18 +163,22 @@ namespace OhMyDog_API.Controllers
                     return BadRequest();
 
                 string queryString = $"SELECT codPet FROM Pet WHERE codPet = '{pet.codPet}'";
-                var reader = ExecutaQuery(queryString);
+                var table = ExecutaQuery(queryString);
 
-                if (!reader.Read())
+                if (table.Rows.Count == 0)
                     return BadRequest("Pet não encontrado!");
 
                 queryString = $"UPDATE Pet SET " +
                     $"Sexo = '{pet.Sexo}', " +
                     $"Cor = '{pet.Cor}', " +
                     $"Nome = '{pet.Nome}', " +
-                    $"Especie = '{pet.Especie}' " +
+                    $"Especie = '{pet.Especie}', " +
+                    $"Observacao = '{pet.Observacao}', " +
+                    $"Castrado = '{pet.Castrado}', " +
+                    $"Peso = '{pet.Peso.Replace(",", ".")}' " +
                     $"WHERE codPet = '{pet.codPet}' ";
-                ExecutaQuery(queryString);
+
+                ExecutaQuery(queryString.Replace("''", "null"));
 
                 return Ok();
             }
