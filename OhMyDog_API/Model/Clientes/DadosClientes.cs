d8c3722355b9ffacd3dd41ef9fad87de8949f45b
DaniloGabrielGuedes <88410319+velocirary@users.noreply.github.com>
2024-02-13 18:53:27 -0300

    fix(DadosCliente) Inserção de campos restantes
    Inserção de campos restantes Cliente

--- OhMyDog_API/Model/Clientes/DadosClientes.cs
+++ OhMyDog_API/Model/Clientes/DadosClientes.cs
@@ -8,11 +8,13 @@
 
         public string CPF { get; set; }
 
-        public DateTime? DataNasc { get; set; }
+        public string DataNasc { get; set; }
 
         public string CEP { get; set; }
 
         public string Logradouro { get; set; }
+        
+        public string Numero { get; set; }
 
         public string Bairro { get; set;}
 
@@ -25,6 +27,30 @@
         public string Senha { get; set; }
 
         public string Celular { get; set; }
+    }
+
+    public class AtualizarCliente
+    {
+        public string CodCliente { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string DataNasc { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Municipio { get; set; }
+
+        public string Estado { get; set; }
+
     }
 
     public class LoginCliente
